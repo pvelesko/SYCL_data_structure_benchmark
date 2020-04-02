e2bd4cb6861921a53b01892274096c465fe3c6bb
Paulius Velesko <paulius.velesko@intel.com>
2020-04-02 12:51:42 +0000

    added complexAoS, debug, correct

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include "SyclUtil.hpp"
 #include "DataStructures.hpp"
 #include "CL/sycl.hpp"
+#include <complex>
 using namespace cl::sycl;
 
 int main(int argc, char** argv) {
@@ -8,12 +9,14 @@ int main(int argc, char** argv) {
   init();
   usm_allocator<char, usm::alloc::shared> usmallocator(q.get_context(), q.get_device());
   std::allocator<char> stdallocator{};
+  void* voidptr; // for allocating USM space for top-level classes
 
   typedef AoS<decltype(stdallocator), Particle<float>> ParticleAoS;
   typedef AoS<decltype(usmallocator), Particle<float>> ParticleAoSSycl;
   typedef SoA<decltype(usmallocator), float*, float*, float*> ParticleSoASycl;
-  void* voidptr; // for allocating USM space for top-level classes
-  //event e; // capture event for waiting or profiling
+  typedef AoS<decltype(usmallocator), Particle<float>> ParticleAoSSycl;
+
+  typedef AoS<decltype(usmallocator), std::complex<float>> ComplexAoSSycl;
 
   voidptr = static_cast<void*>(usmallocator.allocate(sizeof(ParticleAoSSycl)));
   ParticleAoSSycl* pAoS = new (voidptr) ParticleAoSSycl(usmallocator, n);
@@ -27,7 +30,9 @@ int main(int argc, char** argv) {
   });
   e.wait();
   timer.timeit("pAoS");
+#ifdef DEBUG
   dump(pAoS->data(), "pAoS");
+#endif 
 
   voidptr = static_cast<void*>(usmallocator.allocate(sizeof(ParticleSoASycl)));
   ParticleSoASycl* pSoA = new (voidptr) ParticleSoASycl(usmallocator, n);
@@ -41,6 +46,7 @@ int main(int argc, char** argv) {
   });
   e.wait();
   timer.timeit("pSoA");
+#ifdef DEBUG
   for(int i = 0; i < n; i++) {
     std::cout << "pSoA[" << i << "] = ";
     std::cout << "(" << pSoA->data<0>()[i];
@@ -48,7 +54,21 @@ int main(int argc, char** argv) {
     std::cout << "," << pSoA->data<2>()[i];
     std::cout << ")" << std::endl;
   }
+#endif 
 
-
+  voidptr = static_cast<void*>(usmallocator.allocate(sizeof(ComplexAoSSycl)));
+  ComplexAoSSycl* complexAoS = new (voidptr) ComplexAoSSycl(usmallocator, n);
+  voidptr = NULL;
+ 
+  timer.timeit("complexAoS");
+  e = par_for(n, [=](int i) {
+    complexAoS->data()[i] = std::complex<float>(-1, 1);
+  });
+  e.wait();
+  q.wait_and_throw();
+  timer.timeit("complexAos");
+#ifdef DEBUG
+  dump(complexAoS->data(), "complexAoS");
+#endif
   return 0;
 }
