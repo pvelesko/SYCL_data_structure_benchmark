0b169131e0fadf9da27d446cdd9f823e33bc274f
Paulius Velesko <paulius.velesko@intel.com>
2020-04-02 12:30:09 +0000

    Testing timers

--- test.cpp
+++ test.cpp
@@ -13,31 +13,34 @@ int main(int argc, char** argv) {
   typedef AoS<decltype(usmallocator), Particle<float>> ParticleAoSSycl;
   typedef SoA<decltype(usmallocator), float*, float*, float*> ParticleSoASycl;
   void* voidptr; // for allocating USM space for top-level classes
-  event e; // capture event for waiting or profiling
+  //event e; // capture event for waiting or profiling
 
   voidptr = static_cast<void*>(usmallocator.allocate(sizeof(ParticleAoSSycl)));
   ParticleAoSSycl* pAoS = new (voidptr) ParticleAoSSycl(usmallocator, n);
   voidptr = NULL;
 
+  timer.timeit("pAoS");
   e = par_for(n, [=](int i) {
     pAoS->data()[i].pos_x = 1;
     pAoS->data()[i].pos_y = 2;
     pAoS->data()[i].pos_z = 3;
   });
-
   e.wait();
+  timer.timeit("pAoS");
   dump(pAoS->data(), "pAoS");
 
   voidptr = static_cast<void*>(usmallocator.allocate(sizeof(ParticleSoASycl)));
   ParticleSoASycl* pSoA = new (voidptr) ParticleSoASycl(usmallocator, n);
   voidptr = NULL;
  
+  timer.timeit("pSoA");
   e = par_for(n, [=](int i) {
     pSoA->data<0>()[i] = 1;
     pSoA->data<1>()[i] = 2;
     pSoA->data<2>()[i] = 3;
   });
   e.wait();
+  timer.timeit("pSoA");
   for(int i = 0; i < n; i++) {
     std::cout << "pSoA[" << i << "] = ";
     std::cout << "(" << pSoA->data<0>()[i];
