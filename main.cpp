6042e9a52fd54b3d40b6ede0bb55d63b8c8a2f99
Paulius Velesko <paulius.velesko@intel.com>
2020-04-07 06:13:10 +0000

    expanded tests

--- main.cpp
+++ main.cpp
@@ -73,6 +73,7 @@ RTYPE calc0(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIP
     psi += detValues0[det0[i]] * detValues1[det1[i]];
   return psi;
 }
+
 RTYPE calc1(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIPTR det1) {
   real_type psi_r = 0, psi_i = 0;
   RTYPE psi = 0;
@@ -84,7 +85,7 @@ RTYPE calc1(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIP
   psi = std::complex<real_type>(psi_r, psi_i);
   return psi;
 }
-RTYPE calc2(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIPTR det1) {
+RTYPE calc1_simd(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIPTR det1) {
   real_type psi_r = 0, psi_i = 0;
   RTYPE psi = 0;
 #pragma omp simd reduction(+:psi_r, psi_i) 
@@ -96,10 +97,10 @@ RTYPE calc2(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIP
   psi = std::complex<real_type>(psi_r, psi_i);
   return psi;
 }
-RTYPE calc3(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIPTR det1) {
+RTYPE calc1_simd_ht(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIPTR det1) {
   real_type psi_r = 0, psi_i = 0;
   RTYPE psi = 0;
-#pragma omp parallel for simd reduction(+:psi_r, psi_i) 
+#pragma omp parallel for simd reduction(+:psi_r, psi_i)
   for (int i = 0; i < N; i++) 
   {
     psi_r += detValues0[det0[i]].real() * detValues1[det1[i]].real() - detValues0[det0[i]].imag() * detValues1[det1[i]].imag();
@@ -108,11 +109,87 @@ RTYPE calc3(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIP
   psi = std::complex<real_type>(psi_r, psi_i);
   return psi;
 }
-RTYPE calc4(const int N, CRRPTR realdetValues0, CRRPTR realdetValues1, CRRPTR imagdetValues0, CRRPTR imagdetValues1, CRIPTR det0, CRIPTR det1) {
+RTYPE calc1_simd_ht_schedule(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIPTR det1) {
+  real_type psi_r = 0, psi_i = 0;
+  RTYPE psi = 0;
+#pragma omp parallel for simd reduction(+:psi_r, psi_i) schedule(static, 1)
+  for (int i = 0; i < N; i++) 
+  {
+    psi_r += detValues0[det0[i]].real() * detValues1[det1[i]].real() - detValues0[det0[i]].imag() * detValues1[det1[i]].imag();
+    psi_i += detValues0[det0[i]].real() * detValues1[det1[i]].real() + detValues0[det0[i]].imag() * detValues1[det1[i]].imag();
+  }
+  psi = std::complex<real_type>(psi_r, psi_i);
+  return psi;
+}
+RTYPE calc1_ht_schedule(const int N, RTYPE* detValues0, RTYPE* detValues1, CRIPTR det0, CRIPTR det1) {
+  real_type psi_r = 0, psi_i = 0;
+  RTYPE psi = 0;
+#pragma omp parallel for schedule(static, 1)
+  for (int i = 0; i < N; i++) 
+  {
+    psi_r += detValues0[det0[i]].real() * detValues1[det1[i]].real() - detValues0[det0[i]].imag() * detValues1[det1[i]].imag();
+    psi_i += detValues0[det0[i]].real() * detValues1[det1[i]].real() + detValues0[det0[i]].imag() * detValues1[det1[i]].imag();
+  }
+  psi = std::complex<real_type>(psi_r, psi_i);
+  return psi;
+}
+
+RTYPE calc2(const int N, CRRPTR realdetValues0, CRRPTR realdetValues1, CRRPTR imagdetValues0, CRRPTR imagdetValues1, CRIPTR det0, CRIPTR det1) {
+  RTYPE psi = 0;
+  real_type psi_r = 0;
+  real_type psi_i = 0;
+  for (int i = 0; i < N; i++) 
+  {
+    psi_r += realdetValues0[det0[i]] * realdetValues1[det1[i]] - imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
+    psi_i += realdetValues0[det0[i]] * realdetValues1[det1[i]] + imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
+  }
+  psi = std::complex<real_type>(psi_r, psi_i);
+  return psi;
+}
+RTYPE calc2_simd(const int N, CRRPTR realdetValues0, CRRPTR realdetValues1, CRRPTR imagdetValues0, CRRPTR imagdetValues1, CRIPTR det0, CRIPTR det1) {
+  RTYPE psi = 0;
+  real_type psi_r = 0;
+  real_type psi_i = 0;
+#pragma omp simd reduction(+:psi_r, psi_i) 
+  for (int i = 0; i < N; i++) 
+  {
+    psi_r += realdetValues0[det0[i]] * realdetValues1[det1[i]] - imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
+    psi_i += realdetValues0[det0[i]] * realdetValues1[det1[i]] + imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
+  }
+  psi = std::complex<real_type>(psi_r, psi_i);
+  return psi;
+}
+RTYPE calc2_simd_ht(const int N, CRRPTR realdetValues0, CRRPTR realdetValues1, CRRPTR imagdetValues0, CRRPTR imagdetValues1, CRIPTR det0, CRIPTR det1) {
   RTYPE psi = 0;
   real_type psi_r = 0;
   real_type psi_i = 0;
 #pragma omp parallel for simd reduction(+:psi_r, psi_i) 
+  for (int i = 0; i < N; i++) 
+  {
+    psi_r += realdetValues0[det0[i]] * realdetValues1[det1[i]] - imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
+    psi_i += realdetValues0[det0[i]] * realdetValues1[det1[i]] + imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
+  }
+  psi = std::complex<real_type>(psi_r, psi_i);
+  return psi;
+}
+RTYPE calc2_simd_ht_schedule(const int N, CRRPTR realdetValues0, CRRPTR realdetValues1, CRRPTR imagdetValues0, CRRPTR imagdetValues1, CRIPTR det0, CRIPTR det1) {
+  RTYPE psi = 0;
+  real_type psi_r = 0;
+  real_type psi_i = 0;
+#pragma omp parallel for simd reduction(+:psi_r, psi_i) schedule(static, 1)
+  for (int i = 0; i < N; i++) 
+  {
+    psi_r += realdetValues0[det0[i]] * realdetValues1[det1[i]] - imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
+    psi_i += realdetValues0[det0[i]] * realdetValues1[det1[i]] + imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
+  }
+  psi = std::complex<real_type>(psi_r, psi_i);
+  return psi;
+}
+RTYPE calc2_ht_schedule(const int N, CRRPTR realdetValues0, CRRPTR realdetValues1, CRRPTR imagdetValues0, CRRPTR imagdetValues1, CRIPTR det0, CRIPTR det1) {
+  RTYPE psi = 0;
+  real_type psi_r = 0;
+  real_type psi_i = 0;
+#pragma omp parallel for schedule(static, 1)
   for (int i = 0; i < N; i++) 
   {
     psi_r += realdetValues0[det0[i]] * realdetValues1[det1[i]] - imagdetValues0[det0[i]] * imagdetValues1[det1[i]];
@@ -145,7 +222,6 @@ RTYPE calc3_sycl(const int N, CRRPTR realdetValues0, CRRPTR realdetValues1, CRRP
   psi = std::complex<real_type>(psi_r, psi_i);
   return psi;
 }
-double t0, t1, t2, t3, t4, t5;
 RTYPE psiref;
 
 template<class Lambda, class... Args>
@@ -201,11 +277,19 @@ int main(int argc, char** argv) {
 
 
   psiref = calc0(N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
-  t0 = bench(calc0, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
-  t1 = bench(calc1, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
-  t2 = bench(calc2, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
-  t3 = bench(calc3, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
-  t4 = bench(calc4, N, mycSoA0->data<0>(), mycSoA1->data<0>(), mycSoA0->data<1>(), mycSoA1->data<1>(), mycSoA0->data<2>(), mycSoA1->data<2>());
+  auto t0  = bench(calc0, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
+
+  auto t1  = bench(calc1, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
+  auto t2  = bench(calc1_simd, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
+  auto t3  = bench(calc1_simd_ht, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
+  auto t4  = bench(calc1_simd_ht_schedule, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
+  auto t5  = bench(calc1_ht_schedule, N, cSoA0->data<0>(), cSoA1->data<0>(), cSoA0->data<1>(), cSoA1->data<1>());
+ 
+  auto t6  = bench(calc2, N, mycSoA0->data<0>(), mycSoA1->data<0>(), mycSoA0->data<1>(), mycSoA1->data<1>(), mycSoA0->data<2>(), mycSoA1->data<2>());
+  auto t7  = bench(calc2_simd, N, mycSoA0->data<0>(), mycSoA1->data<0>(), mycSoA0->data<1>(), mycSoA1->data<1>(), mycSoA0->data<2>(), mycSoA1->data<2>());
+  auto t8  = bench(calc2_simd_ht, N, mycSoA0->data<0>(), mycSoA1->data<0>(), mycSoA0->data<1>(), mycSoA1->data<1>(), mycSoA0->data<2>(), mycSoA1->data<2>());
+  auto t9  = bench(calc2_simd_ht_schedule, N, mycSoA0->data<0>(), mycSoA1->data<0>(), mycSoA0->data<1>(), mycSoA1->data<1>(), mycSoA0->data<2>(), mycSoA1->data<2>());
+  auto t10 = bench(calc2_ht_schedule, N, mycSoA0->data<0>(), mycSoA1->data<0>(), mycSoA0->data<1>(), mycSoA1->data<1>(), mycSoA0->data<2>(), mycSoA1->data<2>());
   //auto t5 = bench(calc3_sycl, N, mycSoA0->data<0>(), mycSoA1->data<0>(), mycSoA0->data<1>(), mycSoA1->data<1>(), mycSoA0->data<2>(), mycSoA1->data<2>());
 
   int num_t;
@@ -216,13 +300,19 @@ int main(int argc, char** argv) {
   }
   std::cout << "-------------- RESULT -------------------" << std::endl;
   std::cout << "OpenMP Threads: " << num_t << std::endl;
-  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0    << "Runtime Baseline (Test0)" <<  std::endl;
-  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t0 << "Speedup Test0 std::complex complex operators" <<  std::endl;
-  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t1 << "Speedup Test1 std::Complex Real/Imag" << std::endl;
-  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t2 << "Speedup Test2 std::Complex Real/Imag SIMD" <<  std::endl;
-  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t3 << "Speedup Test3 std::Complex Real/Imag SIMD HT" <<  std::endl;
-  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t4 << "Speedup Test4 MyComplex    Real/Imag SIMD HT" <<  std::endl;
-  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t5 << "Speedup Test5 MyComplex    Real/Imag SIMD HT SYCL" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0     << "Runtime Baseline (Test0)" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t0  << "Speedup Test0  std::complex complex operators" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t1  << "Speedup Test1  std::Complex Real/Imag" << std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t2  << "Speedup Test2  std::Complex Real/Imag SIMD" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t3  << "Speedup Test3  std::Complex Real/Imag SIMD HT" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t4  << "Speedup Test4  std::Complex Real/Imag SIMD HT schedule" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t5  << "Speedup Test5  std::Complex Real/Imag HT schedule" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t6  << "Speedup Test6  MyComplex    Real/Imag" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t7  << "Speedup Test7  MyComplex    Real/Imag SIMD" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t8  << "Speedup Test8  MyComplex    Real/Imag SIMD HT" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t9  << "Speedup Test9  MyComplex    Real/Imag SIMD HT schedule" <<  std::endl;
+  std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t10 << "Speedup Test10 MyComplex    Real/Imag HT schedule" <<  std::endl;
+  //std::cout << std::left << std::setprecision(3) << std::setw(10) << t0/t5 << "Speedup Test5 MyComplex    Real/Imag SIMD HT SYCL" <<  std::endl;
 
   return 0;
 }
